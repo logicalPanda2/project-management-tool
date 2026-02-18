e7f2991ab2281f5e86b1dc1a12479c7f7b61ae57
Marcel <marcelrg.business@gmail.com>
2026-02-18 23:39:11 +0700

    frontend: Decrease header font size for small screens

--- frontend/src/layouts/Root.tsx
+++ frontend/src/layouts/Root.tsx
@@ -4,7 +4,7 @@ export default function Root() {
     return (
         <div className="flex flex-col flex-nowrap min-h-screen relative">
             <header className="flex flex-col md:flex-row flex-nowrap justify-between md:items-center sticky top-0 px-12 py-6 bg-white z-10 border-b">
-                <h1 className="text-4xl mb-6 md:mb-0">Project Management Tool</h1>
+                <h1 className="text-3xl md:text-4xl mb-6 md:mb-0">Project Management Tool</h1>
                 <nav className="border-t md:border-0 pt-6 md:pt-0">
                     <ul className="flex flex-row flex-nowrap items-center gap-4 md:gap-8">
                         <li>
