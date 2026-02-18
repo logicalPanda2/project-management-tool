b3fb65f94d18dba9a446f34e95211a1279e54038
Marcel <marcelrg.business@gmail.com>
2026-02-18 23:30:50 +0700

    frontend: Change spacing on not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
     return (
         <main className="min-h-screen p-12">
-            <h1 className="text-4xl mb-8">404 Not Found</h1>
-            <p className="text-2xl mb-4">The page you were looking for does not exist.</p>
+            <h1 className="text-4xl mb-4">404 Not Found</h1>
+            <p className="text-2xl mb-8">The page you were looking for does not exist.</p>
             <Link to={"/"} className="text-2xl underline hover:no-underline focus-visible:outline-0 focus-visible:no-underline">Back to Home</Link>
         </main>
     );
