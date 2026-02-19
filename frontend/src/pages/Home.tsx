b66d5c2ee6c94a07264bae25ddf8dee84c6eff6b
Marcel <marcelrg.business@gmail.com>
2026-02-19 21:07:45 +0700

    frontend: Add placeholder project to home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,38 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    const projects: Project[] = []; // some role based API fetch that gets all projects
+    const projects: Project[] = [{
+        title: "Placeholder project",
+        description: "this is just a placeholder, folks. Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+        status: "INCOMPLETE",
+        tasks: [
+            {
+                title: "Task 1",
+                status: "INCOMPLETE",
+                id: crypto.randomUUID(),
+            },
+            {
+                title: "Task 2",
+                status: "INCOMPLETE",
+                id: crypto.randomUUID(),
+            },
+            {
+                title: "Task 3",
+                status: "INCOMPLETE",
+                id: crypto.randomUUID(),
+            },
+        ],
+        id: crypto.randomUUID(),
+    }]; // some role based API fetch that gets all projects
 
     return (
         projects.length > 0 
         ? projects.map((project) => (
             <div className="max-w-2xl md:w-4/5 mb-6 p-6 border rounded-lg relative" key={project.title}>
                 <p className="text-2xl mb-2">{project.title}</p>
-                <p className="mb-6">{project.description}</p>
-                <div className={`absolute top-6 right-6 border w-4 h-4 rounded-full ${project.status === "INCOMPLETE" ? "bg-black" : "bg-white"}`}></div>
-                <button className="bg-black text-white rounded focus-visible:outline-0 focus-visible:bg-neutral-900 hover:bg-neutral-900 active:bg-neutral-800 px-3 py-1">View</button>
+                <p className="mb-1">{project.description}</p>
+                <p className="mb-6">This project is <span className="font-semibold">{project.status.toLowerCase()}</span>.</p>
+                <Link to={"project"} className="bg-black text-white rounded focus-visible:outline-0 focus-visible:bg-neutral-900 hover:bg-neutral-900 active:bg-neutral-800 px-3 py-1">View</Link>
             </div>
         ))
         : <>
