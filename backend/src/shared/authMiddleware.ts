96f209b26f8b9c0c67cb0986f67ecf2074ad38cb
Marcel <marcelrg.business@gmail.com>
2026-02-26 18:14:38 +0700

    backend: Add auth middleware

---
+++ backend/src/shared/authMiddleware.ts
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import type { Request, Response } from "express";
+
+dotenv.config();
+
+export default function auth(
+	req: Request,
+	res: Response,
+	next: (...params: any[]) => any,
+) {
+	if (!process.env.JWT_ACCESS_SECRET)
+		throw new Error(
+			"Secret access key not configured. Check .env.example for more information.",
+		);
+
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader) return res.sendStatus(401);
+
+	const token = authHeader.split(" ")[1];
+
+	if (!token) return res.sendStatus(401);
+
+	try {
+		const user = jwt.verify(
+			token,
+			process.env.JWT_ACCESS_SECRET,
+		) as User;
+		req.user = user;
+		next();
+	} catch (e) {
+		return res.sendStatus(401);
+	}
+
+    return undefined;
+}
