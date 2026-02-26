999fa024f5469d670cec847000bd40ae495f11b3
Marcel <marcelrg.business@gmail.com>
2026-02-26 17:55:39 +0700

    backend: Strengthen error handling in db config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -11,12 +11,25 @@ if(
     !process.env.DB_NAME
 ) throw new Error("Incomplete env variables. Check .env.example for more information.");
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-});
+let pool: Pool | null = null;
+
+try {
+    pool = new Pool({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+    });
+} catch(e) {
+    if(
+        typeof e === "object" && 
+        e !== null &&
+        "message" in e &&
+        typeof e.message === "string"
+    ) throw new Error(`Error: ${e.message}. Have you started your database?`);
+
+    throw new Error("An unexpected error occured.");
+}
 
 export default pool;
