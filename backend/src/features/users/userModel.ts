afa39e7388a2d0898140e6bc465d2d89290a1507
Marcel <marcelrg.business@gmail.com>
2026-02-28 10:06:24 +0700

    backend: Add new query to get user data by email

---
+++ backend/src/features/users/userModel.ts
@@ -0,0 +1,10 @@
+import pool from "../../config/db.js";
+
+export async function getUserByEmail(email: string): Promise<User> {
+    const result = await pool?.query(
+        `SELECT id, email, password FROM users WHERE email = $1;`,
+        [email]
+    );
+
+    return result?.rows[0];
+}
