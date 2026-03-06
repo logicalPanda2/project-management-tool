4a5741e0acca9911a57b134f99010a05202f1039
Marcel <marcelrg.business@gmail.com>
2026-03-06 22:23:15 +0700

    Move db logic to model file

--- backend/src/features/tasks/taskModel.ts
+++ backend/src/features/tasks/taskModel.ts
@@ -10,7 +10,23 @@ export async function addNewTask(task: Task, projectId: string) {
     );
 }
 
-export async function addNewTasks(query: string, data: string[]) {
+export async function addNewTasks(tasks: Task[], projectId: string) {
+    function createInsertQueryForNTasks(taskArrLen: number) {
+        const baseQuery = "INSERT INTO tasks (id, title, status, project_id) VALUES";
+        const placeholders = [];
+        const placeholdersPerInsert = 4;
+
+        for(let i = 1; i <= taskArrLen; i++) {
+            const n = placeholdersPerInsert * i;
+            placeholders.push(`($${n - 3}, $${n - 2}, $${n - 1}, $${n})`);
+        }
+
+        return `${baseQuery}${placeholders.join(",")};`;
+    }
+
+    const query = createInsertQueryForNTasks(tasks.length);
+    const data = tasks.flatMap(t => [t.id, t.title, t.status, projectId]);
+
     await pool?.query(
         query,
         data
