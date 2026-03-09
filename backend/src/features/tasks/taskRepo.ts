5a5ab6062bda7810facc580b42f4b762db4704dc
Marcel <marcelrg.business@gmail.com>
2026-03-09 18:31:54 +0700

    Add update fn for tasks

--- backend/src/features/tasks/taskRepo.ts
+++ backend/src/features/tasks/taskRepo.ts
@@ -1,7 +1,7 @@
 import pool from "../../config/db.js";
 import addBulkInsertPlaceholders from "../../shared/utils/addBulkInsertPlaceholders.js";
 
-export async function addNewTask(task: Task, projectId: string) {
+export async function create(task: Task, projectId: string) {
     await pool?.query(
         `
         INSERT INTO tasks (id, title, status, project_id)
@@ -11,12 +11,20 @@ export async function addNewTask(task: Task, projectId: string) {
     );
 }
 
-export async function addNewTasks(tasks: Task[], projectId: string) {
+export async function createMany(tasks: Task[], projectId: string) {
     const query = addBulkInsertPlaceholders("INSERT INTO tasks (id, title, status, project_id) VALUES", 4, tasks.length);
     const data = tasks.flatMap(t => [t.id, t.title, t.status, projectId]);
 
     await pool?.query(
         query,
         data
+    );
+}
+
+export async function updateById(id: string, newTask: Task) {
+    await pool?.query(
+        `UPDATE tasks SET title = $1, status = $2
+        WHERE id = $3;`,
+        [newTask.title, newTask.status, id]
     );
 }
