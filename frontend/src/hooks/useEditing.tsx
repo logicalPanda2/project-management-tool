a84cec72b8fa8ab057716b3afd8da1ad3abf5a95
Marcel <marcelrg.business@gmail.com>
2026-03-11 18:34:25 +0700

    Extract crude hook

---
+++ frontend/src/hooks/useEditing.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function useEditing() {
+    const [title, setTitle] = useState<string>("");
+    const [titleErr, setTitleErr] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [descriptionErr, setDescriptionErr] = useState<string>("");
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [taskErr, setTaskErr] = useState<string>("");
+    const [userEmail, setUserEmail] = useState<string>("");
+    const [userCounter, setUserCounter] = useState<number>(0);
+    const [userErr, setUserErr] = useState<string>("");
+    const [userEmails, setUserEmails] = useState<User[]>([]);
+
+    return {
+        title, setTitle,
+        titleErr, setTitleErr,
+        description, setDescription,
+        descriptionErr, setDescriptionErr,
+        tasks, setTasks,
+        taskErr, setTaskErr,
+        userEmail, setUserEmail,
+        userCounter, setUserCounter,
+        userErr, setUserErr,
+        userEmails, setUserEmails
+    };
+}
--- frontend/src/pages/CreateEdit.tsx
+++ frontend/src/pages/CreateEdit.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import useEditing from "../hooks/useEditing";
 
 export default function CreateEdit() {
-	const [title, setTitle] = useState<string>("");
-	const [titleErr, setTitleErr] = useState<string>("");
-	const [description, setDescription] = useState<string>("");
-	const [descriptionErr, setDescriptionErr] = useState<string>("");
-	const [tasks, setTasks] = useState<Task[]>([]);
-	const [taskErr, setTaskErr] = useState<string>("");
-	const [userEmail, setUserEmail] = useState<string>("");
-    const [userCounter, setUserCounter] = useState<number>(0);
-	const [userErr, setUserErr] = useState<string>("");
-	const [userEmails, setUserEmails] = useState<User[]>([]);
+	const {
+        title, setTitle,
+        titleErr, setTitleErr,
+        description, setDescription,
+        descriptionErr, setDescriptionErr,
+        tasks, setTasks,
+        taskErr, setTaskErr,
+        userEmail, setUserEmail,
+        userCounter, setUserCounter,
+        userErr, setUserErr,
+        userEmails, setUserEmails
+    } = useEditing();
 
 	const addNewTask = (
 		e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
