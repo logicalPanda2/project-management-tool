1c9c224f9704b6fdab87c3b01aa809f9f52f6e33
Marcel <marcelrg.business@gmail.com>
2026-03-12 17:04:45 +0700

    Organize type declarations

--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -1,33 +1,25 @@
-type Status = "COMPLETE" | "INCOMPLETE";
-type UserRole = "CONTRIBUTOR" | "CREATOR";
-
-interface Task {
-	title: string;
-	status: Status;
-	id: string;
-}
-
+// PROJECTS
+type Status = "COMPLETE" | "INCOMPLETE"
 interface Project {
 	title: string;
 	description: string;
 	status: Status;
 	id: string;
 }
-
-interface ProjectMetadata {
+interface Task {
 	title: string;
-	description: string;
 	status: Status;
 	id: string;
 }
-
-interface User {
-	email: string;
-	id: string;
-}
-
 interface ProjectComment {
 	user: string;
 	title: string;
+	id: string;
+}
+
+// USERS
+type UserRole = "CONTRIBUTOR" | "CREATOR";
+interface User {
+	email: string;
 	id: string;
 }
