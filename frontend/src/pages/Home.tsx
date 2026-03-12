d247873291103aa0489b1572c66469f7fb6a5124
Marcel <marcelrg.business@gmail.com>
2026-03-12 23:29:36 +0700

    style: add svg arrow to view button

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -29,9 +29,12 @@ export default function Home() {
                 <div className="hover:transform-[translateY(-1px)] transition-custom-all w-fit">
                     <Link
                         to={`project/${project.id}`}
-                        className="bg-gradient shadow-default text-primary px-4 py-1.5 rounded-lg active:shadow-pressed active:bg-gradient-pressed active:text-secondary focus-visible:outline-1 transition-custom-all hover:text-secondary"
+                        className="bg-gradient shadow-default text-primary px-4 py-1.5 rounded-lg active:shadow-pressed active:bg-gradient-pressed active:text-secondary focus-visible:outline-1 transition-custom-all hover:text-accent flex flex-row flex-nowrap items-center stroke-neutral-800/50 hover:stroke-accent"
                     >
                         View
+                        <svg className="fill-none stroke-inherit stroke-[1.5px] inline-block w-4 ml-1 mt-0.5" viewBox="0 0 24 24">
+                            <path d="M5 12h14M13 6l6 6-6 6"/>
+                        </svg>
                     </Link>
                 </div>
 			</div>
