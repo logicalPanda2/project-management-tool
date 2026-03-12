2f4cc3f435f76ad3e1af3577bbcff3faad883994
Marcel <marcelrg.business@gmail.com>
2026-03-12 16:57:22 +0700

    Make hook only have business logic

--- frontend/src/hooks/useMembers.tsx
+++ frontend/src/hooks/useMembers.tsx
@@ -1,45 +1,22 @@
 import { useState } from "react";
 
 export default function useMembers(initialEmails: User[] = [], memberCount: number = 0) {
-	const [emailField, setEmailField] = useState<string>("");
 	const [count, setCount] = useState<number>(memberCount);
-	const [emailFieldErr, setEmailFieldErr] = useState<string>("");
 	const [emails, setEmails] = useState<User[]>(initialEmails);
 
-	const add = (): void => {
-		setEmailFieldErr("");
-
-		if (!emailField.trim()) {
-			setEmailFieldErr("Cannot be empty");
-			return;
-		}
-
-		if (
-			!emailField.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-		) {
-			setEmailFieldErr("Invalid email pattern");
-			return;
-		}
-
+	const add = (email: string): void => {
 		setEmails([
 			...emails,
 			{
-				email: emailField,
+				email: email,
 				id: count,
 			},
 		]);
 
-		setEmailField("");
 		setCount((c) => c + 1);
 	};
 
 	const edit = (user: User, email: string): void => {
-		setEmailFieldErr("");
-
-		if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
-			setEmailFieldErr("Invalid email pattern");
-		}
-
 		setEmails([
 			...emails.map((u) =>
 				u.id === user.id
@@ -57,10 +34,6 @@ export default function useMembers(initialEmails: User[] = [], memberCount: numb
 	};
 
 	return {
-		emailField,
-		setEmailField,
-		emailFieldErr,
-		setEmailFieldErr,
 		add,
 		edit,
 		remove,
