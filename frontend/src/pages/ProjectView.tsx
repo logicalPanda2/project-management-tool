0327a4580023e11c3978f67b050e5ce5b1854e08
Marcel <marcelrg.business@gmail.com>
2026-03-13 19:16:17 +0700

    fix: remove task empty state

--- frontend/src/pages/ProjectView.tsx
+++ frontend/src/pages/ProjectView.tsx
@@ -76,38 +76,31 @@ export default function ProjectView() {
 					<header>
 						<h2 className="text-2xl mb-5">Tasks</h2>
 					</header>
-					{tasks &&
-					tasks.list.length > 0 ? (
-						tasks.list.map((t) => (
-							<div
-								className="flex flex-col justify-between items-start relative mb-4 max-w-lg p-4 rounded-lg bg-default shadow-default hover:shadow-bold-hover transition-custom-all"
-								key={t.id}
-							>
-								<div className="flex sm:flex-row flex-col justify-between w-full items-start flex-nowrap mb-8 sm:mb-5 gap-4 sm:gap-0">
-                                    <p className="text-primary text-lg max-w-full sm:max-w-2/3">
-                                        {t.title}
-                                    </p>
-                                    <p className={`flex flex-row flex-nowrap items-center rounded-xl font-semibold text-[13px] shadow-pressed bg-gradient px-2.5 py-[2.5px] ${t.status === "INCOMPLETE" ? "text-neutral-800/50" : "text-success"}`}>
-                                        <span className={`rounded-full w-1.5 h-1.5 inline-block mr-2 ${t.status === "INCOMPLETE" ? "bg-neutral-800/40" : "bg-text-success"}`}></span>
-                                        {t.status}
-                                    </p>
-                                </div>
-                                <button
-                                    className="bg-gradient shadow-default px-3 py-1.5 rounded-lg active:shadow-pressed active:bg-gradient-pressed active:text-secondary focus-visible:outline-1 transition-custom-all hover:text-success-dark hover:transform-[translateY(-1px)] text-success text-sm font-semibold stroke-success hover:stroke-success-dark"
-                                    onClick={() => tasks.editStatus(t, t.status === "COMPLETE" ? "INCOMPLETE" : "COMPLETE")}
-                                >
-                                    <svg className="fill-none stroke-inherit stroke-[1.5px] inline-block w-4 mr-2 mb-0.5" viewBox="0 0 24 24">
-                                        <polyline points="20 6 9 17 4 12"/>
-                                    </svg>
-                                    Done
-                                </button>
-							</div>
-						))
-					) : (
-						<p className="text-xl text-neutral-900">
-							There are no tasks yet.
-						</p>
-					)}
+					{tasks.list.map((t) => (
+                        <div
+                            className="flex flex-col justify-between items-start relative mb-4 max-w-lg p-4 rounded-lg bg-default shadow-default hover:shadow-bold-hover transition-custom-all"
+                            key={t.id}
+                        >
+                            <div className="flex sm:flex-row flex-col justify-between w-full items-start flex-nowrap mb-8 sm:mb-5 gap-4 sm:gap-0">
+                                <p className="text-primary text-lg max-w-full sm:max-w-2/3">
+                                    {t.title}
+                                </p>
+                                <p className={`flex flex-row flex-nowrap items-center rounded-xl font-semibold text-[13px] shadow-pressed bg-gradient px-2.5 py-[2.5px] ${t.status === "INCOMPLETE" ? "text-neutral-800/50" : "text-success"}`}>
+                                    <span className={`rounded-full w-1.5 h-1.5 inline-block mr-2 ${t.status === "INCOMPLETE" ? "bg-neutral-800/40" : "bg-text-success"}`}></span>
+                                    {t.status}
+                                </p>
+                            </div>
+                            <button
+                                className="bg-gradient shadow-default px-3 py-1.5 rounded-lg active:shadow-pressed active:bg-gradient-pressed active:text-secondary focus-visible:outline-1 transition-custom-all hover:text-success-dark hover:transform-[translateY(-1px)] text-success text-sm font-semibold stroke-success hover:stroke-success-dark"
+                                onClick={() => tasks.editStatus(t, t.status === "COMPLETE" ? "INCOMPLETE" : "COMPLETE")}
+                            >
+                                <svg className="fill-none stroke-inherit stroke-[1.5px] inline-block w-4 mr-2 mb-0.5" viewBox="0 0 24 24">
+                                    <polyline points="20 6 9 17 4 12"/>
+                                </svg>
+                                Done
+                            </button>
+                        </div>
+                    ))}
 				</section>
 				<section className="mb-8">
 					<header>
