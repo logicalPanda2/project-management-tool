ec58cdaca9745797f757183d96f6ec8fd8dca74f
Marcel <marcelrg.business@gmail.com>
2026-03-15 09:37:44 +0700

    feat: add loading spinner

---
+++ frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+export default function LoadingSpinner() {
+    return (
+        <div className="w-full flex items-center justify-center grow mb-12">
+            {
+                /*! Source code below by Zongbin (https://github.com/nzbin) */
+                /*! Copyright (C) 2018 nzbin, MIT License */
+            }
+            <div className="dot-pulse"></div>
+        </div>
+    );
+}
