001c2383a700d3495496c12cad5e8883468ead41
Marcel <marcelrg.business@gmail.com>
2026-03-17 11:52:03 +0700

    fix: enable updating task status on edit page

--- frontend/src/pages/ProjectEdit.tsx
+++ frontend/src/pages/ProjectEdit.tsx
@@ -340,7 +340,7 @@ function Content({
                                     className="bg-gradient shadow-default px-3 py-1.5 rounded-lg active:shadow-pressed active:bg-gradient-pressed active:text-secondary focus-visible:outline-1 transition-custom-all hover:text-success-dark hover:transform-[translateY(-1px)] text-success text-sm font-semibold stroke-success hover:stroke-success-dark"
                                     onClick={(e) => {
                                         e.preventDefault();
-                                        tasks.editStatus(t, t.status === "COMPLETE" ? "INCOMPLETE" : "COMPLETE");
+                                        tasks.editStatus(t, paramsId, t.status === "COMPLETE" ? "INCOMPLETE" : "COMPLETE");
                                     }}
                                 >
                                     <svg className="fill-none stroke-inherit stroke-[1.5px] inline-block w-4 mr-2 mb-0.5" viewBox="0 0 24 24">
